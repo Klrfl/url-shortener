2fc7bed659dfd18d33413c947efa0f8f90b7242e
Efraim Munthe <efraimmunthe101@gmail.com>
2023-03-30 20:00:04 +0700

    added components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,28 @@
-<script setup></script>
 <template>
-  <h1>URL Shortener</h1>
-  <p>
-    insert the url you want to shorten and click shorten to get the shortened
-    url.
-  </p>
+  <TheHeader @gotShortUrl="handleUrl" />
+  <Results :shortUrl="shortUrl" v-if="shortUrl.length !== 0" />
 </template>
+
+<script>
+import TheHeader from "../components/TheHeader.vue";
+import Results from "../components/Results.vue";
+
+export default {
+  components: {
+    TheHeader,
+    Results,
+  },
+
+  data() {
+    return {
+      shortUrl: "",
+    };
+  },
+
+  methods: {
+    handleUrl(url) {
+      this.shortUrl = url;
+    },
+  },
+};
+</script>
