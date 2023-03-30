b808a73ae2565506e497234ac48c7dbc895efa78
Efraim Munthe <efraimmunthe101@gmail.com>
2023-03-30 20:32:23 +0700

    added method for copying url

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="results">
     <h2>Here is your shortened url!</h2>
-    <a :href="shortUrl">{{ shortUrl }}</a>
-    <button>Copy URL</button>
+    <a class="short-url" :href="shortUrl" target="_blank">{{ shortUrl }}</a>
+    <button @click="copyUrl">Copy URL</button>
   </div>
 </template>
 
 <script>
 export default {
+  name: "Results",
   props: {
     shortUrl: String,
+  },
+
+  methods: {
+    copyUrl() {
+      navigator.clipboard.writeText(this.shortUrl);
+    },
   },
 };
 </script>
