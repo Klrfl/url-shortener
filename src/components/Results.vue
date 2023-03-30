ca1a919ba2a3efbda13c74ada8e39cc9e47624ce
Efraim Munthe <efraimmunthe101@gmail.com>
2023-03-30 19:59:33 +0700

    initialize base component

---
+++ src/components/Results.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="results">
+    <h2>Here is your shortened url!</h2>
+    <a :href="shortUrl">{{ shortUrl }}</a>
+    <button>Copy URL</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    shortUrl: String,
+  },
+};
+</script>
