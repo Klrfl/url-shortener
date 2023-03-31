e59e13b4fcc774197d15bcd1ae6944983d8615af
Efraim Munthe <efraimmunthe101@gmail.com>
2023-03-31 15:16:16 +0700

    make component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <ul class="nav-links">
+      <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+      <li><router-link :to="{ name: 'About' }">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+nav {
+  outline: 2px solid #333;
+}
+.nav-links {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+.nav-links a {
+  display: block;
+  color: inherit;
+  text-decoration: none;
+  padding: 1rem;
+  position: relative;
+}
+
+.nav-links a.current-page::before {
+  content: "";
+  background: rgb(39, 83, 180);
+
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 0.125rem;
+  width: 100%;
+}
+
+.nav-links a:hover {
+  background: #333;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <TheNavbar />
+  <main>
+    <h1>About this project</h1>
+    <p>This project</p>
+  </main>
+</template>
+
+<script>
+import TheNavbar from "../components/TheNavbar.vue";
+
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
+
+<style>
+main {
+  padding: 2rem;
+}
+</style>
