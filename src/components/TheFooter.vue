2a35c50d13fa2cea176e2cf0b4266711847bd851
Efraim Munthe <efraimmunthe101@gmail.com>
2023-04-01 16:26:02 +0700

    make footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <p>
+      Made by <a href="https://github.com/Klrfl/" target="_blank">Klrfl</a>,
+      2023. See
+      <a href="https://github.com/Klrfl/url-shortener">
+        this project on Github
+      </a>
+    </p>
+  </footer>
+</template>
+
+<style>
+footer {
+  background: #22242d;
+  padding: 2rem;
+}
+</style>
