abb9e2bf7e740599cdd2da72b1f4d31418fdcdc3
Efraim Munthe <efraimmunthe101@gmail.com>
2023-04-01 12:31:09 +0700

    button state

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import TheNavbar from "./components/TheNavbar.vue";
+import TheFooter from "./components/TheFooter.vue";
 import "./assets/globals.css";
 </script>
 
 <template>
   <TheNavbar />
   <RouterView />
+  <TheFooter />
 </template>
 
 <style>
 .btn {
-  padding: 0.5rem;
+  padding: 0.5rem 1rem;
   cursor: pointer;
 }
 
 main {
   padding: 2rem;
-  margin: 0 auto;
+  margin: auto;
   max-width: 70ch;
 }
 </style>
